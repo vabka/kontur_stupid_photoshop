d1711eda013702804b0482698f8811975eb82b15
Vasiliy Bannikov <vab-999@ya.ru>
2020-09-01 01:17:32 +0500

    Write tests for convertors

---
+++ KonturCourse.StupidPhotos.Tests/AssertBitmap.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using Xunit;
+
+namespace KonturCourse.StupidPhotos.Tests
+{
+    public static class AssertBitmap
+    {
+        public static void SamePixels(Bitmap expected, Bitmap actual)
+        {
+            Assert.Equal(expected.Width, actual.Width);
+            Assert.Equal(expected.Height, actual.Height);
+
+            for (var x = 0; x < expected.Width; x++)
+            for (var y = 0; y < expected.Height; y++)
+            {
+                var expectedPixel = expected.GetPixel(x, y);
+                var actualPixel = actual.GetPixel(x, y);
+                Assert.Equal(expectedPixel.R, actualPixel.R);
+                Assert.Equal(expectedPixel.G, actualPixel.G);
+                Assert.Equal(expectedPixel.B, actualPixel.B);
+                Assert.Equal(expectedPixel.A, actualPixel.A);
+            }
+        }
+    }
+}
---
+++ KonturCourse.StupidPhotos.Tests/ConvertorTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using KonturCourse.StupidPhotos.Lib.Data;
+using Xunit;
+
+namespace KonturCourse.StupidPhotos.Tests
+{
+    public class ConvertorTests
+    {
+        [Fact]
+        public void Bidirectional()
+        {
+            using var cat = Image.FromFile("cat.jpg");
+            using var originalBitmap = new Bitmap(cat);
+
+            var photo = Convertors.Bitmap2Photo(originalBitmap);
+            using var newBitmap = Convertors.Photo2Bitmap(photo);
+            AssertBitmap.SamePixels(originalBitmap, newBitmap);
+        }
+    }
+}
