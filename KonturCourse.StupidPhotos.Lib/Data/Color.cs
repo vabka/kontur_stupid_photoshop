ab15c7b982ac30d5dffc3490183a02256af7613f
Vasiliy Bannikov <vab-999@ya.ru>
2020-09-01 05:07:09 +0500

    Check invariants in Color construction

--- KonturCourse.StupidPhotos.Lib/Data/Color.cs
+++ KonturCourse.StupidPhotos.Lib/Data/Color.cs
@@ -1,9 +1,17 @@
-﻿namespace KonturCourse.StupidPhotos.Lib.Data
+﻿using System;
+
+namespace KonturCourse.StupidPhotos.Lib.Data
 {
     public readonly struct Color
     {
         public Color(double red, double green, double blue)
         {
+            if (red < 0 || red > 1)
+                throw new ArgumentOutOfRangeException(nameof(red));
+            if (green < 0 || green > 1)
+                throw new ArgumentOutOfRangeException(nameof(green));
+            if (blue < 0 || blue > 1)
+                throw new ArgumentOutOfRangeException(nameof(blue));
             Red = red;
             Green = green;
             Blue = blue;
