84511d5b0b5264a797ca63c1c105a6fd31cc29c9
Vasiliy Bannikov <vab-999@ya.ru>
2020-09-01 05:08:06 +0500

    Check invariants in Point construction

--- KonturCourse.StupidPhotos.Lib/Data/Pixel.cs
+++ KonturCourse.StupidPhotos.Lib/Data/Pixel.cs
@@ -1,8 +1,19 @@
-﻿namespace KonturCourse.StupidPhotos.Lib.Data
+﻿using System;
+
+namespace KonturCourse.StupidPhotos.Lib.Data
 {
     public readonly struct Point
     {
-        public Point(int x, int y) => (X, Y) = (x, y);
+        public Point(int x, int y)
+        {
+            if (x < 0)
+                throw new ArgumentOutOfRangeException(nameof(x));
+            if (y < 0)
+                throw new ArgumentOutOfRangeException(nameof(y));
+            X = x;
+            Y = y;
+        }
+
         public int X { get; }
         public int Y { get; }
         public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
@@ -16,6 +27,7 @@
         public Color Color { get; }
 
         public void Deconstruct(out int x, out int y) => (x, y) = Point;
+
         public void Deconstruct(out int x, out int y, out double r, out double g, out double b) =>
             ((x, y), (r, g, b)) = (Point, Color);
     }
