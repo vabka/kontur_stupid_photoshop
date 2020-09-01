290026d8c4ea413d8f7a984cfcf5e157c591c915
Vasiliy Bannikov <vab-999@ya.ru>
2020-09-01 05:16:49 +0500

    Split file to classes

--- KonturCourse.StupidPhotos.Lib/Data/Pixel.cs
+++ KonturCourse.StupidPhotos.Lib/Data/Pixel.cs
@@ -1,24 +1,5 @@
-﻿using System;
-
-namespace KonturCourse.StupidPhotos.Lib.Data
+﻿namespace KonturCourse.StupidPhotos.Lib.Data
 {
-    public readonly struct Point
-    {
-        public Point(int x, int y)
-        {
-            if (x < 0)
-                throw new ArgumentOutOfRangeException(nameof(x));
-            if (y < 0)
-                throw new ArgumentOutOfRangeException(nameof(y));
-            X = x;
-            Y = y;
-        }
-
-        public int X { get; }
-        public int Y { get; }
-        public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
-    }
-
     public readonly struct Pixel
     {
         public Pixel(Point point, Color color) => (Point, Color) = (point, color);
---
+++ KonturCourse.StupidPhotos.Lib/Data/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KonturCourse.StupidPhotos.Lib.Data
+{
+    public readonly struct Point
+    {
+        public Point(int x, int y)
+        {
+            if (x < 0)
+                throw new ArgumentOutOfRangeException(nameof(x));
+            if (y < 0)
+                throw new ArgumentOutOfRangeException(nameof(y));
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+        public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
+    }
+}
